1757101646ca10324a56dad810f287f04bf5f4b4
Vinicius Leal Alves <59040683+lealalves@users.noreply.github.com>
2025-09-08 20:23:35 -0300

    feature: adiciona cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
-import { RolesGuard } from './common/guards/roles.guard';
-import { CatchExceptionFilter } from './common/filter/catch-exception.filter';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.URL_FRONTEND,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
