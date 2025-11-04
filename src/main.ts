96788a9102a304dadcdd2975c3ad32352ccfd9d3
Kawamotus <124678308+Kawamotus@users.noreply.github.com>
2025-11-04 19:27:28 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.URL_FRONTEND,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
