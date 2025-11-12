18a0fb433e7bdc13b49f36d0deb09fc0802748ea
Vinicius Leal Alves <59040683+lealalves@users.noreply.github.com>
2025-11-11 21:53:01 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ import { CatchExceptionFilter } from './common/filter/catch-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'https://cattus.vercel.app',
-      'http://localhost:5173'
-    ],
-    credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://cattus.vercel.app',
+  //     'http://localhost:5173'
+  //   ],
+  //   credentials: true,
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
 
 
   app.useGlobalPipes(
